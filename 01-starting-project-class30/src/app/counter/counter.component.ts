188bed4b50349f517ce097b83f64345b3d3a9239
Steffan Troxel <sjtroxel@alumni.iu.edu>
2025-04-16 20:26:02 -0500

    oh so we have signals

--- 01-starting-project-class30/src/app/counter/counter.component.ts
+++ 01-starting-project-class30/src/app/counter/counter.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, inject, NgZone, OnInit, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, signal } from '@angular/core';
 
 import { InfoMessageComponent } from '../info-message/info-message.component';
 
@@ -8,10 +8,9 @@ import { InfoMessageComponent } from '../info-message/info-message.component';
   templateUrl: './counter.component.html',
   styleUrl: './counter.component.css',
   imports: [InfoMessageComponent],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CounterComponent implements OnInit {
-  private zone = inject(NgZone);
   count = signal(0);
 
   get debugOutput() {
@@ -24,11 +23,10 @@ export class CounterComponent implements OnInit {
       this.count.set(0);
     }, 4000);
 
-    this.zone.runOutsideAngular(() => {
-      setTimeout(() => {
-        console.log('Timer Expired!!');
-      }, 5000);
-    });
+    setTimeout(() => {
+      console.log('Timer Expired!!');
+    }, 5000);
+    
 
   }
 
--- 01-starting-project-class30/src/app/messages/messages-list/messages-list.component.ts
+++ 01-starting-project-class30/src/app/messages/messages-list/messages-list.component.ts
@@ -1,5 +1,6 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MessagesService } from '../messages.service';
+
 
 @Component({
   selector: 'app-messages-list',
@@ -8,22 +9,10 @@ import { MessagesService } from '../messages.service';
   styleUrl: './messages-list.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MessagesListComponent implements OnInit {
+export class MessagesListComponent {
   private messagesService = inject(MessagesService);
-  private cdRef = inject(ChangeDetectorRef);
-  private destroyRef = inject(DestroyRef);
+  messages = this.messagesService.allMessages;
 
- messages: string[] = [];
-
-  ngOnInit() {
-    const subscription = this.messagesService.messages$.subscribe((messages) => {
-      this.messages = messages;
-      this.cdRef.markForCheck();
-    });
-      this.destroyRef.onDestroy(() => {
-        subscription.unsubscribe();
-      })
-  }
 
   get debugOutput() {
     console.log('[MessagesList] "debugOutput" binding re-evaluated.');
